1b2d2579a111dda512d75d32464b36657b17710b
Ashish Pratap <68291149+AP8953@users.noreply.github.com>
2025-01-12 23:56:07 +0530

    Time: 19 ms (100.00%) | Memory: 67.9 MB (99.29%) - LeetSync

--- 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.java
+++ 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.java
@@ -1,29 +1,41 @@
 class Solution {
+    Set<Integer> vis = new HashSet<>();
+
     public int findMaximumXOR(int[] nums) {
-        int max = 0;
-        int mask = 0;
-        
-        // Traverse from the most significant bit (31st bit) to the least significant bit (0th bit)
-        for (int i = 31; i >= 0; i--) {
-            mask |= (1 << i); // Create a mask that includes the first i bits
-            HashSet<Integer> prefixes = new HashSet<>();
-            
-            // Add all the prefixes of the current numbers
-            for (int num : nums) {
-                prefixes.add(num & mask);
-            }
-            
-            // Try to find the maximum XOR for the current bit
-            int candidate = max | (1 << i); // Set the current bit in the candidate result
-            for (int prefix : prefixes) {
-                // Check if there exists a prefix that can give the candidate XOR
-                if (prefixes.contains(prefix ^ candidate)) {
-                    max = candidate; // Update max if a better candidate is found
-                    break;
-                }
-            }
+        int max = 0, ans = 0, mask = 0, masks = 0;
+
+        for(int num : nums) {
+            max = Math.max(max, num);
         }
         
-        return max;
+        int bit = 31;
+
+        while(bit-- >= 0) {
+            if((max >> bit & 1) == 1) break;
+        }
+
+        for(int i = bit; i >= 0; i--) {
+            mask = 1 << i;
+            masks |= mask;
+
+            if(check(nums, masks, ans | mask)) {
+                ans |= mask;
+            }
+            vis.clear();
+        }
+        return ans;
+    }
+
+    private boolean check(int[] nums, int masks, int ans) {
+        
+        for(int num : nums) {
+            num &= masks;
+            
+            if(vis.contains(num ^ ans)) {
+                return true;
+            }
+            vis.add(num);
+        }
+        return false;
     }
 }
