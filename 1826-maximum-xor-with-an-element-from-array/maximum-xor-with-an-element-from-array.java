3f1f7d4b3c31dd14df93492a33ca149de364a1ba
Ashish Pratap <68291149+AP8953@users.noreply.github.com>
2025-01-15 00:53:20 +0530

    Time: 136 ms (73.61%) | Memory: 126.2 MB (48.33%) - LeetSync

--- 1826-maximum-xor-with-an-element-from-array/maximum-xor-with-an-element-from-array.java
+++ 1826-maximum-xor-with-an-element-from-array/maximum-xor-with-an-element-from-array.java
@@ -1,99 +1,94 @@
-import java.util.Arrays;
-import java.util.Comparator;
-
-class Node {
-    Node[] links = new Node[2];
-
-    boolean containsKey(int index) {
-        return links[index] != null;
-    }
-
-    Node get(int index) {
-        return links[index];
-    }
-
-    void put(int index, Node node) {
-        links[index] = node;
-    }
-}
-
-class Trie {
-    private static Node root;
-
-    Trie() {
-        root = new Node();
-    }
-
-    public void insert(int num) {
-        Node node = root;
-        for (int i = 31; i >= 0; i--) {
-            int bit = (num >> i) & 1;
-            if (!node.containsKey(bit)) {
-                node.put(bit, new Node());
-            }
-            node = node.get(bit);
-        }
-    }
-
-    public int getMax(int num) {
-        Node node = root;
-        if (node == null) return -1;
-
-        int max = 0;
-        for (int i = 31; i >= 0; i--) {
-            int bit = (num >> i) & 1;
-            if (node.containsKey(1 - bit)) {
-                max |= (1 << i);
-                node = node.get(1 - bit);
-            } else if (node.containsKey(bit)) {
-                node = node.get(bit);
-            } else {
-                return -1;
-            }
-        }
-        return max;
-    }
-}
-
 class Solution {
-    public int[] maximizeXor(int[] nums, int[][] queries) {
-        Arrays.sort(nums); // Sort nums for controlled trie insertion
+    // Define a node structure for a trie
+    static class Node {
+        Node[] links = new Node[2];
 
-        // Include query indices for reordering after sorting
-        int[][] queriesWithIndex = new int[queries.length][3];
-        for (int i = 0; i < queries.length; i++) {
-            queriesWithIndex[i][0] = queries[i][0];
-            queriesWithIndex[i][1] = queries[i][1];
-            queriesWithIndex[i][2] = i; // Store original index
+        boolean containsKey(int ind) {
+            return links[ind] != null;
         }
 
-        // Sort queries by `mi` (second element in each query)
-        Arrays.sort(queriesWithIndex, Comparator.comparingInt(a -> a[1]));
+        Node get(int ind) {
+            return links[ind];
+        }
+
+        void put(int ind, Node node) {
+            links[ind] = node;
+        }
+    }
+
+    // Trie data structure for handling binary representations of numbers
+    static class Trie {
+        Node root;
+
+        Trie() {
+            root = new Node();
+        }
+
+        void insert(int num) {
+            Node node = root;
+            for (int i = 31; i >= 0; i--) {
+                int bit = (num >> i) & 1;
+                if (!node.containsKey(bit)) {
+                    node.put(bit, new Node());
+                }
+                node = node.get(bit);
+            }
+        }
+
+        int findMax(int num) {
+            Node node = root;
+            int maxNum = 0;
+            for (int i = 31; i >= 0; i--) {
+                int bit = (num >> i) & 1;
+                if (node.containsKey(1 - bit)) {
+                    maxNum |= (1 << i);
+                    node = node.get(1 - bit);
+                } else {
+                    node = node.get(bit);
+                }
+            }
+            return maxNum;
+        }
+    }
+
+    public int[] maximizeXor(int[] nums, int[][] queries) {
+        // Array to store the results
+        int[] ans = new int[queries.length];
+
+        // List to store offline queries as pairs of (maxValue, (number, queryIndex))
+        List<int[]> offlineQueries = new ArrayList<>();
+        for (int i = 0; i < queries.length; i++) {
+            offlineQueries.add(new int[]{queries[i][1], queries[i][0], i});
+        }
+
+        // Sort nums and offline queries based on their maxValue
+        Arrays.sort(nums);
+        offlineQueries.sort((a, b) -> Integer.compare(a[0], b[0]));
 
         Trie trie = new Trie();
-        int[] result = new int[queries.length];
-        int numIndex = 0;
+        int idx = 0;
+        int n = nums.length;
 
         // Process each query
-        for (int[] query : queriesWithIndex) {
-            int xi = query[0];
-            int mi = query[1];
+        for (int[] query : offlineQueries) {
+            int maxValue = query[0];
+            int number = query[1];
             int queryIndex = query[2];
 
-            // Insert all nums less than or equal to mi into the trie
-            while (numIndex < nums.length && nums[numIndex] <= mi) {
-                trie.insert(nums[numIndex]);
-                numIndex++;
+            // Insert elements into Trie that are ≤ maxValue
+            while (idx < n && nums[idx] <= maxValue) {
+                trie.insert(nums[idx]);
+                idx++;
             }
 
-            // Get the max XOR for xi if there are valid numbers in the trie
-            if (numIndex == 0) {
-                result[queryIndex] = -1; // No valid numbers
+            // If no numbers have been inserted, the result is -1
+            if (idx == 0) {
+                ans[queryIndex] = -1;
             } else {
-                result[queryIndex] = trie.getMax(xi);
+                ans[queryIndex] = trie.findMax(number);
             }
         }
 
-        return result;
+        return ans;
     }
 }
