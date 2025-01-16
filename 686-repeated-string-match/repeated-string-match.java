da99727e33a7959ca6d84a675c1613e23522ac31
Ashish Pratap <68291149+AP8953@users.noreply.github.com>
2025-01-16 17:20:14 +0530

    Time: 1 ms (100.00%) | Memory: 42.2 MB (84.52%) - LeetSync

--- 686-repeated-string-match/repeated-string-match.java
+++ 686-repeated-string-match/repeated-string-match.java
@@ -1,20 +1,33 @@
 class Solution {
-    public int repeatedStringMatch(String a, String b) {
-    int count = 1;
-    String repeated = a;
-    while (repeated.length() < b.length()) {
-        repeated += a;
-        count++;
+   public int repeatedStringMatch(String A, String B) {
+        
+        
+        char[] a = A.toCharArray();
+        char[] b = B.toCharArray();
+        for(int i=0;i<a.length;i++){
+            int len = loop(a,b,i);
+            if(len > 0){// 
+                int count = 1;
+                
+                len = B.length() - a.length + i;
+                count += len/a.length;
+                count += len%a.length > 0 ? 1 : 0;
+                return count;
+            }else if(len + a.length <= 0){
+                return -1;
+            }
+        }
+        return -1;
+        
     }
-    if (repeated.contains(b)) {
-        return count;
+    public int loop(char[] a,char[] b,int start){
+        int count = start;
+        for(char c : b){
+            if(a[start % a.length] != c){
+                return count - start;
+            }
+            start++;
+        }
+        return 1; 
     }
-    repeated += a;
-    count++;
-    if (repeated.contains(b)) {
-        return count;
-    }
-    return -1;
-}
-
 }
