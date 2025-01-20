cb925e6bf0900538b332daffd7d7b9206b54355a
Ashish Pratap <68291149+AP8953@users.noreply.github.com>
2025-01-20 21:36:57 +0530

    Time: 1 ms (24.64%) | Memory: 41.7 MB (46.95%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,5 +1,49 @@
 class Solution {
+    private int[] calculateLPS(String needle){
+        int lps[]=new int[needle.length()];
+        int j=0;
+        for(int i=1;i<needle.length();){
+            if(needle.charAt(i)==needle.charAt(j)){
+                j+=1;
+                lps[i]=j;
+                i+=1;
+            }else{
+                if(j!=0){
+                    j=lps[j-1];
+                }
+                else{
+                    lps[i]=0;
+                    i+=1;
+                }
+            }
+        }
+        return lps;
+    }
     public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
+        if(needle.isEmpty()) return 0;
+        int[] lps=calculateLPS(needle);
+        int i=0,j=0;
+        while(i<haystack.length()){
+            if(haystack.charAt(i)==needle.charAt(j)){
+                i+=1;
+                j+=1;
+            }
+            if(j==needle.length()){
+                return i-j;
+
+            }
+            
+                if(i<haystack.length() && needle.charAt(j)!=haystack.charAt(i)){
+                    if(j!=0){
+                        j=lps[j-1];
+                    }else{
+                        i+=1;
+                    }
+                
+                
+                
+            }
+        }
+        return -1;
     }
 }
