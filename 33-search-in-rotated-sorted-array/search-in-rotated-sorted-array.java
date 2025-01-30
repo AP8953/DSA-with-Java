84b0f5ae5e8b9181eb894b275b8f2663e48f4442
Ashish Pratap <68291149+AP8953@users.noreply.github.com>
2025-01-31 04:00:00 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (12.02%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,22 +1,47 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int low=0,high=nums.length-1;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(nums[mid]==target) return mid;
-            if(nums[low]<=nums[mid]){
-                if(nums[low]<=target && target <= nums[mid]){
-                    high=mid-1;
-                }
-                else low=mid+1;
-            }else{
-                if(nums[mid]<=target && target<= nums[high]){
-                    low=mid+1;
-                }
-                else{
-                    high=mid-1;
-                }
+        int pivot = findPivot(nums);
+
+        // If target is found at pivot
+        if (nums[pivot] == target) return pivot;
+
+        int n = nums.length;
+        
+        // If array is not rotated, do normal binary search
+        if (pivot == 0) {
+            return binarySearch(nums, 0, n - 1, target);
+        }
+
+        // Binary search in left or right part
+        if (target >= nums[0]) {
+            return binarySearch(nums, 0, pivot - 1, target);
+        } else {
+            return binarySearch(nums, pivot, n - 1, target);
+        }
+    }
+
+    // Function to find the index of the smallest element (pivot)
+    private int findPivot(int[] nums) {
+        int low = 0, high = nums.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] > nums[high]) {
+                low = mid + 1;  // Pivot is in the right half
+            } else {
+                high = mid;  // Pivot is in the left half
             }
-        }return -1;
+        }
+        return low;
+    }
+
+    // Standard Binary Search
+    private int binarySearch(int[] nums, int low, int high, int target) {
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] < target) low = mid + 1;
+            else high = mid - 1;
+        }
+        return -1;
     }
 }
