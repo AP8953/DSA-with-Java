758dac2f4d6ecd93c38b79134dafb4c4b9b27bf9
Ashish Pratap <68291149+AP8953@users.noreply.github.com>
2025-01-31 04:22:28 +0530

    Time: 31 ms (58.32%) | Memory: 51.6 MB (51.95%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,32 +1,33 @@
+import java.util.*;
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> ans=new ArrayList<>();
-        
-        for(int i=0;i<nums.length-2;i++){
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            // Skip duplicate elements for i
             if (i > 0 && nums[i] == nums[i - 1]) continue;
-            int target=0-nums[i];
-            int first=i+1;
-            int last=nums.length-1;
-            while(first<last){
-                if((nums[first]+nums[last])==target){
-                    List<Integer> temp=new ArrayList<>();
-                    temp.add(nums[first]);
-                    temp.add(nums[last]);
-                    temp.add(nums[i]);
-                    ans.add(temp);
+
+            int target = -nums[i];
+            int first = i + 1, last = nums.length - 1;
+
+            while (first < last) {
+                int sum = nums[first] + nums[last];
+
+                if (sum == target) {
+                    ans.add(Arrays.asList(nums[i], nums[first], nums[last]));
+
+                    // Skip duplicate elements for first and last
                     while (first < last && nums[first] == nums[first + 1]) first++;
                     while (first < last && nums[last] == nums[last - 1]) last--;
+
                     first++;
                     last--;
-                }
-                else {
-                    if(nums[first]+nums[last]>target){
-                        last--;
-                    }
-                    else{
-                        first++;
-                    }
+                } else if (sum > target) {
+                    last--;
+                } else {
+                    first++;
                 }
             }
         }
