7bd1a991d887d7216a731e78726da6a323fa2da3
Ashish Pratap <68291149+AP8953@users.noreply.github.com>
2025-02-05 21:13:26 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (49.67%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -1,11 +1,15 @@
 class Solution {
+    
+    public void helper(int[] ans,int count,int n){
+        if(n>=ans.length) return;
+        ans[n] = count;
+        n=n<<1;
+        helper(ans,count+1,n+1);
+        helper(ans,count,n);
+    }
     public int[] countBits(int n) {
-        int[] ans= new int[n+1];
-        for(int i=1;i<=n;i++){
-            //ans[i]=hammingWeight(i);
-            ans[i] = ans[i >> 1] + (i & 1);
-        }
+        int ans[] = new int[n+1];
+        helper(ans,1,1);
         return ans;
     }
-    
 }
