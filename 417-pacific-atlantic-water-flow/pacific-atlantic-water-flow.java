21afa0dbd4eb306e456e56d68d254c1e13a005e6
Ashish Pratap <68291149+AP8953@users.noreply.github.com>
2025-02-08 14:43:56 +0530

    Time: 4 ms (97.73%) | Memory: 45.3 MB (93.48%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
@@ -1,72 +1,37 @@
 class Solution {
-    int m,n;
-    int[][] matrix;
-    public List<List<Integer>> pacificAtlantic(int[][] matrix) {
-        List<List<Integer>> result=new ArrayList<>();
-        if(matrix.length==0 || matrix[0].length==0) return result;
-        m=matrix.length;
-        n=matrix[0].length;
-        this.matrix=matrix;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-				//If both flags are true, add in result
-                if(reachPacific(i,j) && reachAtlantic(i,j)){
-                    result.add(Arrays.asList(i,j));
+    int[][] dir={{0,1},{0,-1},{-1,0},{1,0}};
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        List<List<Integer>> ans=new ArrayList<>();
+        if(heights.length==0||heights[0].length==0) return null;
+
+        int row=heights.length;
+        int col=heights[0].length;
+        boolean[][] pacific=new boolean[row][col];
+        boolean[][] atlantic=new boolean[row][col];
+
+        for(int i=0;i<col;i++){
+            dfs(heights, 0,i, Integer.MIN_VALUE, pacific);
+            dfs(heights, row-1,i, Integer.MIN_VALUE, atlantic);
+        }
+        for(int i=0;i<row;i++){
+            dfs(heights, i, 0, Integer.MIN_VALUE, pacific);
+            dfs(heights, i, col-1, Integer.MIN_VALUE, atlantic);
+        }
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(pacific[i][j] && atlantic[i][j]){
+                    ans.add(Arrays.asList(i,j));
                 }
             }
         }
-        return result;
+        return ans;
     }
-    
-    boolean reachPacific(int r,int c){
-        
-        if(r==0 || c==0) return true;
-        if(matrix[r][c]==-1) return false;
-        int val=matrix[r][c];
-        matrix[r][c]=-1;
-        boolean flag=false;
-		    
-        //Call DFS in all 4 Directions 
-        if(matrix[r-1][c]<=val){
-            flag=reachPacific(r-1,c);
+    private void dfs(int[][] heights, int i, int j, int prev, boolean[][] ocean){
+        if(i<0 || i>=heights.length || j<0 || j>=heights[0].length) return;
+        if(heights[i][j]<prev || ocean[i][j]) return;
+        ocean[i][j]=true;
+        for(int[] d:dir){
+            dfs(heights, i+d[0], j+d[1], heights[i][j], ocean);
         }
-        
-        if(!flag && matrix[r][c-1]<=val){
-            flag = reachPacific(r,c-1);
-        }
-        
-        if(!flag && c+1<n && matrix[r][c+1]<=val){
-            flag = reachPacific(r,c+1);
-        }
-        
-        if(!flag && r+1<m && matrix[r+1][c]<=val){
-            flag = reachPacific(r+1,c);
-        }
-        matrix[r][c]=val;
-        return flag;
-    }
-    
-    boolean reachAtlantic(int r,int c){
-        if(r==m-1 || c==n-1) return true;
-        if(matrix[r][c]==-1) return false;
-        int val=matrix[r][c];
-        matrix[r][c]=-1;
-        boolean flag=false;
-		    
-        //Call DFS in all 4 Directions
-        if(matrix[r+1][c]<=val){
-            flag=reachAtlantic(r+1,c);
-        }
-        if(!flag &&  matrix[r][c+1]<=val){
-            flag = reachAtlantic(r,c+1);
-        }
-        if(!flag && c-1>=0 &&  matrix[r][c-1]<=val){
-            flag = reachAtlantic(r,c-1);
-        }
-        if(!flag &&  r-1>=0 && matrix[r-1][c]<=val){
-            flag = reachAtlantic(r-1,c);
-        }
-        matrix[r][c]=val;
-        return flag; 
     }
 }
