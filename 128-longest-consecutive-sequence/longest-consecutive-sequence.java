0aceefe3425c84f364b7fb60f8cbea601a5b7bea
Ashish Pratap <68291149+AP8953@users.noreply.github.com>
2025-02-09 01:39:38 +0530

    Time: 67 ms (18.56%) | Memory: 88.5 MB (6.04%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,20 +1,31 @@
+import java.util.*;
+
 class Solution {
+    private Set<Integer> numSet;
+    private Set<Integer> visited;
+    
     public int longestConsecutive(int[] nums) {
-        Set<Integer> set=new HashSet<>();
-        int max=0;
-        for(int num: nums) set.add(num);
-        for(int num:set){
-            if(!set.contains(num-1)){
-                int next=num+1;
-                int count=1;
-                while(set.contains(next)){
-                    next++;
-                    count++;
-                }
-                max=Math.max(max,count);
+        if (nums.length == 0) return 0;
+
+        numSet = new HashSet<>();
+        visited = new HashSet<>();
+        
+        for (int num : nums) numSet.add(num);
+
+        int maxLen = 0;
+        for (int num : numSet) {
+            if (!visited.contains(num)) {  // Start DFS only for unvisited nodes
+                maxLen = Math.max(maxLen, dfs(num));
             }
         }
-        return max;
-
+        return maxLen;
+    }
+    
+    private int dfs(int num) {
+        if (!numSet.contains(num) || visited.contains(num)) return 0;
+        
+        visited.add(num); // Mark as visited
+        
+        return 1 + dfs(num + 1) + dfs(num - 1); // Explore both directions
     }
 }
