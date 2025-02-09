edb2e9b0470826b5df4602a71679d61e9f5af97b
Ashish Pratap <68291149+AP8953@users.noreply.github.com>
2025-02-09 17:20:02 +0530

    Time: 2 ms (37.22%) | Memory: 44.6 MB (97.70%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,22 +1,41 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> ans = new ArrayList<>();
-        int i=0;
-        while(i<intervals.length && intervals[i][1]<newInterval[0]){
-            ans.add(intervals[i]);
-            i+=1;
-        }
-        while(i<intervals.length && intervals[i][0]<=newInterval[1]){
+        // [[1, 3], [5, 6]], [7, 8]
+        // [[1, 3], [5, 6], [7, 8]]
 
-            newInterval[0]=Math.min(newInterval[0],intervals[i][0]);
-            newInterval[1]=Math.max(newInterval[1],intervals[i][1]);
-            i+=1;
+        List<int[]> result = new ArrayList<>();
+        // intervals ends before the newInterval starts
+        int i = 0;
+        while(i < intervals.length) {
+            if(intervals[i][1] >= newInterval[0]) {
+                break;
+            }
+            result.add(intervals[i]);
+            i += 1;
         }
-        ans.add(newInterval);
-        while(i<intervals.length){
-            ans.add(intervals[i]);
-            i+=1;
+        // intervals (starts before the newInterval starts and) ends after the newInterval starts
+        // merge required
+        while(
+            i < intervals.length &&
+            intervals[i][0] <= newInterval[1] &&
+            intervals[i][1] >= newInterval[0]
+        ) {
+            newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+            newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
+            i += 1;
         }
-        return ans.toArray(new int[ans.size()][]);
+        result.add(newInterval);
+
+        // intervals starts after the newInterval ends
+        while(i < intervals.length) {
+            result.add(intervals[i]);
+            i += 1;
+        }
+
+        int[][] ans = new int[result.size()][2];
+        for(int j = 0; j < ans.length; j++) {
+            ans[j] = result.get(j);
+        }
+        return ans;
     }
 }
