6af51d36778f2291fd468a1099a992d1ee540fe6
Ashish Pratap <68291149+AP8953@users.noreply.github.com>
2025-02-24 01:52:56 +0530

    Time: 5 ms (74.25%) | Memory: 58.2 MB (31.62%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,18 +1,14 @@
 class Solution {
     public int maxArea(int[] height) {
-        int first = 0, last = height.length - 1;
-        int max = 0; // No need for Integer.MIN_VALUE, as area is always non-negative
-
-        while (first < last) {
-            int minHeight = Math.min(height[first], height[last]);
-            max = Math.max(max, minHeight * (last - first));
-
-            // Move the pointer pointing to the shorter line
-            if (height[first] < height[last]) {
-                first++;
-            } else {
-                last--;
-            }
+        int first=0;
+        int last=height.length-1;
+        int max=Integer.MIN_VALUE;
+        while(first<last){
+            int min=Math.min(height[last], height[first]);//height[first]>height[last]?height[last]:height[first];
+            int temp=min*(last-first);
+            max=Math.max(max, temp);
+            if(height[first]<height[last]) first++;
+            else last--;
         }
         return max;
     }
