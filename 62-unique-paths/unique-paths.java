3fff92cd40c76425d1593eb5fc2fb0040ae07124
Ashish Pratap <68291149+AP8953@users.noreply.github.com>
2025-03-08 00:20:43 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (45.91%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,11 +1,9 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        long res = 1;
-        int k = Math.min(m - 1, n - 1); // Choose the smaller value for efficiency
-        for (int i = 0; i < k; i++) {
-            res = (res * (m + n - 2 - i)) / (i + 1);
-            System.out.println(res);
+        long ans = 1;
+        for(int i = 1; i < m; i++) {
+            ans = ans * (n - 1 + i) / i;
         }
-        return (int) res;
+        return (int) ans;
     }
 }
