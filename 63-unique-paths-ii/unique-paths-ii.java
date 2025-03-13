d356cb2db10f44ce0f8c1ee1f233cb9781a99e5f
Ashish Pratap <68291149+AP8953@users.noreply.github.com>
2025-03-14 00:35:13 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (38.45%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,24 +1,27 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
     int m = obstacleGrid.length, n = obstacleGrid[0].length;
-    int[][] dp = new int[m][n];
+    int[] prev = new int[n]; // Optimized DP array to store previous row results
     
     for (int i = 0; i < m; i++) {
+        int[] curr = new int[n]; // Current row DP array
         for (int j = 0; j < n; j++) {
             if (obstacleGrid[i][j] == 1) {
-                dp[i][j] = 0; // Obstacle
+                curr[j] = 0; // No path possible if there's an obstacle
             } else if (i == 0 && j == 0) {
-                dp[i][j] = 1; // Start point
+                curr[j] = 1; // Start point has only one way
             } else {
-                int up = (i > 0) ? dp[i - 1][j] : 0;
-                int left = (j > 0) ? dp[i][j - 1] : 0;
+                int up = (i > 0) ? prev[j] : 0; // Paths from above
+                int left = (j > 0) ? curr[j - 1] : 0; // Paths from left
                 
-                dp[i][j] = up + left;
+                curr[j] = up + left; // Total paths to current cell
             }
         }
+        prev = curr; // Update previous row for next iteration
     }
     
-    return dp[m - 1][n - 1];
+    return prev[n - 1]; // Return paths count to the destination cell
 }
+
 
 }
