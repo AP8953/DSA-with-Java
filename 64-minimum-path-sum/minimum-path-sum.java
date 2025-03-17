50a401df4f08df3693fcb5bc1f3a8cd9b1fc6c29
Ashish Pratap <68291149+AP8953@users.noreply.github.com>
2025-03-17 20:20:37 +0530

    Time: 7 ms (6.21%) | Memory: 47.4 MB (89.61%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,13 +1,27 @@
 class Solution {
+
     public int minPathSum(int[][] grid) {
         int m=grid.length;
         int n=grid[0].length;
         int dp[][] =new int[m][n];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
+        // for(int[] row:dp){
+        //     Arrays.fill(row,-1);
 
+        // }
+        // return countSum(m-1,n-1,dp, grid);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) dp[i][j]= grid[i][j];
+                else if(i<0 || j<0) dp[i][j]=Integer.MAX_VALUE;
+                else {
+                    int up = (i > 0) ? countSum(i - 1, j, dp, grid) : Integer.MAX_VALUE;
+                    int left = (j > 0) ? countSum(i, j - 1, dp, grid) : Integer.MAX_VALUE;
+
+                    dp[i][j] = grid[i][j] + Math.min(up, left);
+                }
+            }
         }
-        return countSum(m-1,n-1,dp, grid);
+        return dp[m-1][n-1];
     }
     private int countSum(int i, int j, int[][] dp, int[][] grid){
         if(i==0 && j==0) return grid[i][j];
