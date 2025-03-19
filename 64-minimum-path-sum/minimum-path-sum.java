f57e8f41f91fac980e3d2acd7ef20c3f254965a8
Ashish Pratap <68291149+AP8953@users.noreply.github.com>
2025-03-19 19:45:53 +0530

    Time: 4 ms (63.13%) | Memory: 47.4 MB (89.84%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,52 +1,22 @@
 class Solution {
-
     public int minPathSum(int[][] grid) {
-        int m=grid.length;
-        int n=grid[0].length;
-        int dp[][] =new int[m][n];
-        // for(int[] row:dp){
-        //     Arrays.fill(row,-1);
+        int m = grid.length;
+        int n = grid[0].length;
+        int[] prev = new int[n];
 
-        // }
-        // return countSum(m-1,n-1,dp, grid);
-        // for(int i=0;i<m;i++){
-        //     for(int j=0;j<n;j++){
-        //         if(i==0 && j==0) dp[i][j]= grid[i][j];
-        //         else if(i<0 || j<0) dp[i][j]=Integer.MAX_VALUE;
-        //         else {
-        //             int up = (i > 0) ? countSum(i - 1, j, dp, grid) : Integer.MAX_VALUE;
-        //             int left = (j > 0) ? countSum(i, j - 1, dp, grid) : Integer.MAX_VALUE;
-
-        //             dp[i][j] = grid[i][j] + Math.min(up, left);
-        //         }
-        //     }
-        // }
-        // return dp[m-1][n-1];
-
-        int[] prev=new int[n];
-        for(int i=0;i<m;i++){
-            int[] curr=new int[n];
-            for(int j=0;j<n;j++){
-                if(i==0 && j==0) curr[j]= grid[i][j];
-                else if(i<0 || j<0) curr[j]=Integer.MAX_VALUE;
+        for (int i = 0; i < m; i++) {
+            int[] curr = new int[n];
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) 
+                    curr[j] = grid[i][j];
                 else {
                     int up = (i > 0) ? prev[j] : Integer.MAX_VALUE;
-                    int left = (j > 0) ? curr[j-1] : Integer.MAX_VALUE;
+                    int left = (j > 0) ? curr[j - 1] : Integer.MAX_VALUE;
                     curr[j] = grid[i][j] + Math.min(up, left);
                 }
             }
-            prev=curr;
+            prev = curr;
         }
-        return prev[n-1];
-    }
-    private int countSum(int i, int j, int[][] dp, int[][] grid){
-        if(i==0 && j==0) return grid[i][j];
-        if(i<0 || j<0) return Integer.MAX_VALUE;
-        if (dp[i][j] != -1) return dp[i][j]; // Memoization check
-
-        int up = (i > 0) ? countSum(i - 1, j, dp, grid) : Integer.MAX_VALUE;
-        int left = (j > 0) ? countSum(i, j - 1, dp, grid) : Integer.MAX_VALUE;
-
-        return dp[i][j] = grid[i][j] + Math.min(up, left);
+        return prev[n - 1];
     }
 }
