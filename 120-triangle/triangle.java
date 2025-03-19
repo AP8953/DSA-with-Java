78915be735165b951076dabcdf5461f3d9b7fb5a
Ashish Pratap <68291149+AP8953@users.noreply.github.com>
2025-03-20 00:02:08 +0530

    Time: 4 ms (47.80%) | Memory: 44.1 MB (76.80%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,21 +1,17 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n=triangle.size();
-        int dp[][] = new int[n][n];
-        for (int row[] : dp)
-            Arrays.fill(row, -1);
-        return countSum(triangle, 0,0,dp);
-
+        int[][] dp=new int[n][n];
+        for(int j=0;j<n;j++){
+            dp[n-1][j]=triangle.get(n-1).get(j);
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int down=triangle.get(i).get(j)+dp[i+1][j];
+                int downRight=triangle.get(i).get(j)+dp[i+1][j+1];
+                dp[i][j]=Math.min(down,downRight);
+            }
+        }
+        return dp[0][0];
     }
-    private int countSum(List<List<Integer>> triangle, int i, int j, int[][] dp){
-        if (dp[i][j] != -1)
-            return dp[i][j];
-        int m=triangle.size();
-        if(i==m-1) return triangle.get(m-1).get(j);
-        int down=triangle.get(i).get(j)+countSum(triangle, i+1, j, dp);
-        int downRight=triangle.get(i).get(j)+countSum(triangle,i+1,j+1, dp);
-        dp[i][j] = Math.min(down, downRight);  // Store the result in dp
-        return dp[i][j];
-    }
-    
 }
