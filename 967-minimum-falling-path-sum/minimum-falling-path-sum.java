cc73afb7bd76995d6fa3b0e353805e20ccc28b37
Ashish Pratap <68291149+AP8953@users.noreply.github.com>
2025-03-22 23:12:23 +0530

    Time: 5 ms (74.47%) | Memory: 45.3 MB (75.87%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -2,46 +2,31 @@ class Solution {
     public int minFallingPathSum(int[][] matrix) {
         int n = matrix.length;
         int[][] dp = new int[n][n];
-        // for (int[] row : dp) {
-        //     Arrays.fill(row, -1);
-        // }
-
-        // int mini = Integer.MAX_VALUE;
-        // for (int j = 0; j < n; j++) {
-        //     int ans = minFallPathSum(0, j, matrix, n, dp); // Start from row 0, not n-1
-        //     mini = Math.min(mini, ans);
-        // }
-        // return mini;
+        int[] prev=new int[n];
         for (int j = 0; j < n; j++) {
             dp[0][j] = matrix[0][j];
         }
+        for (int j = 0; j < n; j++) {
+            prev[j] = matrix[0][j];
+        }
         for(int i=1;i<n;i++){
+            int[] curr=new int[n];
             for(int j=0;j<n;j++){
-                int up = dp[i-1][j];  // Coming from top
-                int leftDiagonal = (j > 0) ? dp[i-1][j-1] : Integer.MAX_VALUE;
-                int rightDiagonal = (j < n-1) ? dp[i-1][j+1] : Integer.MAX_VALUE;
+                int up = prev[j];  
+                int leftDiagonal = (j > 0) ? prev[j-1] : Integer.MAX_VALUE;
+                int rightDiagonal = (j < n-1) ? prev[j+1] : Integer.MAX_VALUE;
 
-                dp[i][j] = matrix[i][j] + Math.min(up, Math.min(leftDiagonal, rightDiagonal));
+                curr[j] = matrix[i][j] + Math.min(up, Math.min(leftDiagonal, rightDiagonal));
             }
+            prev=curr;
         }
         int mini = Integer.MAX_VALUE;
         for (int j = 0; j < n; j++) {
-            mini = Math.min(mini, dp[n-1][j]);
+            mini = Math.min(mini, prev[j]);
         }
 
         return mini;
     }
 
-    private int minFallPathSum(int i, int j, int[][] matrix, int n, int[][] dp) {
-        if (j < 0 || j >= n) return (int) Math.pow(10, 9);
-        if (i == n - 1) return matrix[i][j]; // Base case: last row
-        
-        if (dp[i][j] != -1) return dp[i][j];
-
-        int down = minFallPathSum(i + 1, j, matrix, n, dp);
-        int leftDown = minFallPathSum(i + 1, j - 1, matrix, n, dp);
-        int rightDown = minFallPathSum(i + 1, j + 1, matrix, n, dp);
-
-        return dp[i][j] = matrix[i][j] + Math.min(down, Math.min(leftDown, rightDown));
-    }
+    
 }
