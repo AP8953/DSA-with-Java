3aae334549246cfb1e1dd3ad8287c6a1843f4f55
Ashish Pratap <68291149+AP8953@users.noreply.github.com>
2025-04-03 00:33:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.java
+++ Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.java
@@ -31,35 +31,32 @@ class GFG {
 
 
 class Solution {
-    int countPartitions(int[] arr, int d) {
-        // code here
-        int totalSum=0;
-        for(int num:arr){
-            totalSum+=num;
+
+    public int countPartitions(int[] arr, int d) {
+        int totalSum = 0;
+        for (int num : arr) totalSum += num;
+
+        if (totalSum < d || (totalSum - d) % 2 != 0) return 0;
+        int target = (totalSum - d) / 2;
+        return perfectSum(arr, target);
         }
-        if(totalSum-d <0 || (totalSum-d)%2!=0) return 0;
-        int target=(totalSum-d)/2;
+    private int perfectSum(int[] arr, int target){
         int n = arr.length;
+        
         int[][] dp = new int[n + 1][target + 1];
 
-        // Base Case: There is always one way to get sum 0 (empty subset)
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = 1;
-        }
+        // Base case: 1 way to achieve sum 0 (empty subset)
+        for (int i = 0; i <= n; i++) dp[i][0] = 1;
 
-        // Fill the DP table
+        // DP table filling
         for (int i = 1; i <= n; i++) {
             for (int j = 0; j <= target; j++) {
-                // Exclude the current element
-                dp[i][j] = dp[i - 1][j];
-                
-                // Include the current element if possible
-                if (arr[i - 1] <= j) {
-                    dp[i][j] += dp[i - 1][j - arr[i - 1]];
-                }
+                dp[i][j] = dp[i - 1][j]; // Exclude current element
+                if (arr[i - 1] <= j) dp[i][j] += dp[i - 1][j - arr[i - 1]]; // Include current element
             }
         }
-
         return dp[n][target];
+    
     }
 }
+
