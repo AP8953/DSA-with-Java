e6aaa911aa7edd2d1867ccf2d1a7785fe8dcea65
Ashish Pratap <68291149+AP8953@users.noreply.github.com>
2025-04-11 19:30:12 +0530

    Time: 26 ms (74.03%) | Memory: 44.7 MB (52.09%) - LeetSync

--- 2998-count-symmetric-integers/count-symmetric-integers.java
+++ 2998-count-symmetric-integers/count-symmetric-integers.java
@@ -1,43 +1,21 @@
 class Solution {
     public int countSymmetricIntegers(int low, int high) {
-        int count=0;
-        if((low<10&&high<10) || ((low>99&& low<1000)&&(high>99&& high<1000)) || (low>9999&&high>9999)){
-            return 0;
-        }
-        else {
-            
-            for(int i=low;i<=high;i++){
-                if(((i<10) || (i>99&& i<1000) || i>9999)){
-                    
+        int count = 0;
+        for (int i = low; i <= high; i++) {
+            String s = String.valueOf(i);
+            int len = s.length();
+            if (len % 2 == 0) {
+                int mid = len / 2;
+                int leftSum = 0, rightSum = 0;
+                for (int j = 0; j < mid; j++) {
+                    leftSum += s.charAt(j) - '0';
+                    rightSum += s.charAt(j + mid) - '0';
                 }
-                else{
-                    if(check(i)) count++;
+                if (leftSum == rightSum) {
+                    count++;
                 }
-                
             }
         }
         return count;
-    }
-    private boolean check(int i){
-        int length=0;
-        int temp=i;
-        while(temp>0){
-            temp=temp/10;
-            length++;
-        }
-        temp=i;
-        int lastTwo=0,firstTwo=0;
-        for(int j=0;j<length/2;j++){
-            lastTwo=lastTwo+i%10;
-            i=i/10;
-        }
-        for(int j=0;j<length/2;j++){
-            firstTwo=firstTwo+i%10;
-            i=i/10;
-        }
-        if(lastTwo==firstTwo) {
-            System.out.println(temp);
-            return true;
-        }return false;
     }
 }
