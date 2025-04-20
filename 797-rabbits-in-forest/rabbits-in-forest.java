5c781bffbea34bd56545ea8cca16e349e2420792
Ashish Pratap <68291149+AP8953@users.noreply.github.com>
2025-04-21 00:43:54 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (70.99%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.java
+++ 797-rabbits-in-forest/rabbits-in-forest.java
@@ -1,20 +1,16 @@
 class Solution {
     public int numRabbits(int[] answers) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int res = 0;
-
-        for (int ans : answers) {
-            map.put(ans, map.getOrDefault(ans, 0) + 1);
+        int n = answers.length;
+        int[] count = new int[1000];
+        
+        int ans = 0;
+        for(int x:answers){
+            if(++count[x] == 1)
+                ans += x + 1;
+            if(count[x] == x + 1)
+                count[x] = 0;
         }
-
-        for (int x : map.keySet()) {
-            int count = map.get(x);
-            int groupSize = x + 1;
-            // Calculate how many groups needed and multiply by group size
-            int groups = (count + x) / groupSize;
-            res += groups * groupSize;
-        }
-
-        return res;
+        
+        return ans;
     }
 }
