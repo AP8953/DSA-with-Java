1a8935884d2875e3b5524029d94f20172f60042c
Ashish Pratap <68291149+AP8953@users.noreply.github.com>
2025-04-28 00:27:55 +0530

    Time: 2 ms (75.01%) | Memory: 53 MB (60.36%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int majorityElement(int[] arr) {
+        int n = arr.length;
+        int candidate = -1;
+        int count = 0;
+
+        // Find a candidate
+        for (int num : arr) {
+            if (count == 0) {
+                candidate = num;
+                count = 1;
+            } else if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        // Validate the candidate
+        count = 0;
+        for (int num : arr) {
+            if (num == candidate) {
+                count++;
+            }
+        }
+	
+      	// If count is greater than n / 2, return the candidate; otherwise, return -1
+        if (count > n / 2) {
+            return candidate;
+        } else {
+            return -1;
+        }
+    }
+}
