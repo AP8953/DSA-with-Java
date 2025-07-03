59bcfc45cde517efb5c31ba28e7f2fe6ea3c9be0
Ashish Pratap <68291149+AP8953@users.noreply.github.com>
2025-07-03 18:59:43 +0530

    Time: 10 ms (49.39%) | Memory: 48.9 MB (52.40%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -6,7 +6,7 @@ class Solution {
             dp[i][0]=1;
         }
         for(int i=1;i<=n;i++){
-            for(int j=0;j<=amount;j++){
+            for(int j=1;j<=amount;j++){
                 dp[i][j]=dp[i-1][j];
                 if (coins[i - 1]<= j ) {
                     dp[i][j] += dp[i][j - coins[i - 1]];
